260b02045d65f74cab726cc46d32cab5d58de595
Johan Sellström <johan@sellstrom.me>
2020-11-03 13:31:35 +0100

    Booking SwiftUI

---
+++ Sources/BooKit/BookingView.swift
@@ -0,0 +1,44 @@
+//
+//  AvailabilityView.swift
+//  Vagus
+//
+//  Created by Johan Sellström on 2020-10-28.
+//  Copyright © 2020 Advatar Systems. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+public struct BookingView: UIViewControllerRepresentable {
+
+    public typealias UIViewControllerType = UINavigationController
+
+    public init() {}
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    public func makeUIViewController(context: Context) -> UINavigationController {
+        let bookingViewController = BookingViewController()
+        //availabilityViewController.authorizeCalendarAccess()
+        let nav = UINavigationController(rootViewController: bookingViewController)
+        nav.delegate = context.coordinator
+        return nav
+    }
+
+    public func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+
+        print("Children \(uiViewController.children)")
+    }
+
+    public class Coordinator: NSObject, UINavigationControllerDelegate {
+
+        var parent: BookingView
+        init(_ booking: BookingView) {
+            self.parent = booking
+        }
+
+    }
+}
